fe54db5a0886c127a58cd246f8519829288ce85a
dev-null-undefined <martinkos007@gmail.com>
2023-07-02 23:28:07 +0200

    flake.nix: reformated and cleaned up repetetive libs

--- flake.nix
+++ flake.nix
@@ -4,88 +4,95 @@
     devshell.url = "github:numtide/devshell/main";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlays.default ];
-        };
-      in rec {
-        apps = {
-          default = {
-            type = "app";
-            program = self.packages.${system}.ascii-art + "/bin/ascii-art";
-          };
-        };
-
-        defaultApp = {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [devshell.overlays.default];
+      };
+      libs = with pkgs; [
+        libjpeg_original
+        ncurses
+        libpng12
+        zlib
+      ];
+    in rec {
+      apps = {
+        default = {
           type = "app";
           program = self.packages.${system}.ascii-art + "/bin/ascii-art";
         };
+      };
 
-        devShell = pkgs.devshell.mkShell {
-          name = "ascii-art";
-          imports = [ "${devshell}/extra/language/c.nix" ];
-          packages = [ pkgs.glibc pkgs.libcxx pkgs.doxygen pkgs.graphviz ];
+      defaultApp = {
+        type = "app";
+        program = self.packages.${system}.ascii-art + "/bin/ascii-art";
+      };
 
-          language.c = {
-            libraries =
-              [ pkgs.libjpeg_original pkgs.ncurses pkgs.libpng12 pkgs.zlib ];
-            includes =
-              [ pkgs.libjpeg_original pkgs.ncurses pkgs.libpng12 pkgs.zlib ];
-            compiler = pkgs.gcc;
-          };
-          commands = [
-            {
-              name = "compile";
-              category = "c++";
-              help = "Optimized compilation with nearly non logging";
-              command = "${pkgs.gnumake}/bin/make fast";
-            }
-            {
-              name = "debug";
-              category = "c++";
-              help = "Faster compilation with more debug info and logging";
-              command = "${pkgs.gnumake}/bin/make fast-debug";
-            }
-            {
-              name = "clean";
-              category = "c++";
-              help =
-                "Clean binaries (Run this between all debug and release versions)";
-              command = "${pkgs.gnumake}/bin/make clean";
-            }
-            {
-              name = "run";
-              category = "testing";
-              command = "${pkgs.gnumake}/bin/make run";
-            }
-            {
-              name = "docs";
-              category = "c++";
-              help = "Generate documentation";
-              command = "${pkgs.gnumake}/bin/make docs";
-            }
-          ];
-          bash = {
-            interactive = "";
-            extra = ''
-              export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
-              export LIBRARY_PATH="$LD_LIBRARY_PATH"
-            '';
-          };
+      devShell = pkgs.devshell.mkShell {
+        name = "ascii-art";
+        imports = ["${devshell}/extra/language/c.nix"];
+        packages = with pkgs; [glibc libcxx doxygen graphviz];
+
+        language.c = {
+          libraries = libs;
+          includes = libs;
+          compiler = pkgs.gcc;
         };
+        commands = [
+          {
+            name = "compile";
+            category = "c++";
+            help = "Optimized compilation with nearly non logging";
+            command = "${pkgs.gnumake}/bin/make fast";
+          }
+          {
+            name = "debug";
+            category = "c++";
+            help = "Faster compilation with more debug info and logging";
+            command = "${pkgs.gnumake}/bin/make fast-debug";
+          }
+          {
+            name = "clean";
+            category = "c++";
+            help = "Clean binaries (Run this between all debug and release versions)";
+            command = "${pkgs.gnumake}/bin/make clean";
+          }
+          {
+            name = "run";
+            category = "testing";
+            command = "${pkgs.gnumake}/bin/make run";
+          }
+          {
+            name = "docs";
+            category = "c++";
+            help = "Generate documentation";
+            command = "${pkgs.gnumake}/bin/make docs";
+          }
+        ];
+        bash = {
+          interactive = "zsh";
+          extra = ''
+            export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
+            export LIBRARY_PATH="$LD_LIBRARY_PATH"
+          '';
+        };
+      };
 
-        defaultPackage = self.packages.${system}.ascii-art;
+      defaultPackage = self.packages.${system}.ascii-art;
 
-        packages = {
-
-          ascii-art = let
-            inherit system;
-            pname = "ascii-art";
-            version = "v0.1.1";
-          in pkgs.stdenv.mkDerivation {
+      packages = {
+        ascii-art = let
+          inherit system;
+          pname = "ascii-art";
+          version = "v0.1.1";
+        in
+          pkgs.stdenv.mkDerivation {
             pname = pname;
             version = version;
             src = pkgs.fetchFromGitHub {
@@ -95,7 +102,7 @@
               sha256 = "sha256-vjJIxG1CzwwAOQLe1CJZbkxW+U2Qwa1fKd8SfxQskW4=";
               fetchSubmodules = true;
             };
-            buildInputs = [ pkgs.libjpeg_original pkgs.ncurses pkgs.libpng12 ];
+            buildInputs = libs;
             configurePhase = ''
               ${pkgs.gnumake}/bin/make clean
             '';
@@ -114,7 +121,7 @@
               platforms = platforms.linux;
             };
           };
-          default = self.packages.${system}.ascii-art;
-        };
-      });
+        default = self.packages.${system}.ascii-art;
+      };
+    });
 }
